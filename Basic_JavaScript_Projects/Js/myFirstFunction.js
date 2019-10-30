75f17d64a76e938a95ee3e35843e313c509e4443
LukeSheedy <56566556+LukeSheedy@users.noreply.github.com>
2019-10-30 16:15:46 -0700

    Saved Functions
    Text Function

--- Basic_JavaScript_Projects/Js/myFirstFunction.js
+++ Basic_JavaScript_Projects/Js/myFirstFunction.js
@@ -1,5 +1,5 @@
-function myFunction() {
-	var str = "This text is supposed to be green";
-	var res = str.valueOf();  
-	document.getElementById("Green_Text").innerHTML = res;
+function myFirstFunction() {
+	var str= "This text is green";
+	var result = str.fontcolor("green");
+	document.getElementById("Green_Text").innerHTML=result;
 }
