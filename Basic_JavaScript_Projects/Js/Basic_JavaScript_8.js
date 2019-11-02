b72f294301d509022dc873117ac906d701578ba2
LukeSheedy <56566556+LukeSheedy@users.noreply.github.com>
2019-11-02 16:39:45 -0700

    Step 151
    Keep Schooolin!

--- Basic_JavaScript_Projects/Js/Basic_JavaScript_8.js
+++ Basic_JavaScript_Projects/Js/Basic_JavaScript_8.js
@@ -1,8 +1,48 @@
-function my_Sentence(){
-    var part_1 ="I have currently";
-	var part_2 = "made this into";
-	var part_3 = " a complete huge";
-	var part_4 = " massive sentence.";
+function mySentence() {
+    var part_1 =["I have currently"];
+	var part_2 = ["made this into"];
+	var part_3 = [" a complete huge"];
+	var part_4 = [" massive sentence."];
 	var whole_sentence = part_1.concat(part_2,part_3,part_4);
-	document.getElementById("sentence").innderHTML = whole_sentence ;
+	document.getElementById("sentence").innerHTML = whole_sentence ;
+}
+
+function slice_Method() {
+	var Sentence= "All work and no play makes Luke a dull boy.";
+	var Section = Sentence.slice(27,31);
+	document.getElementById("Slice").innerHTML = Section;
+}
+
+function Upper_case(){
+    var Case = "Life is like a Box of Chocolates.";
+    var CaseUp = Case.toUpperCase()
+    document.getElementById("Upper").innerHTML = CaseUp;
+}
+
+function Searching() {
+    var string = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaatheaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"; 
+    var result = string.search("the");
+    document.getElementById("Search").innerHTML = result;
+  }
+
+  function string_Method() {
+	var Y = 182000;
+	document.getElementById("Numbers").innerHTML = Y.toString();
+}
+
+
+function precision_Method() {
+	var Y = 182000.645647984694;
+	document.getElementById("Precision").innerHTML = Y.toPrecision();
+}
+
+function toFixed_method(){
+    var Y =5.3648547;
+    document.getElementById("Fix").innerHTML = Y.toFixed(3)
+}
+
+function value_method (){
+    var H = "Return Orignal value"
+    var R = H.valueOf();
+    document.getElementById("Value").innerHTML = R;
 }
